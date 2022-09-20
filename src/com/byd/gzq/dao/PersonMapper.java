d4e8a353f78a473f6cf7bbf47beb79b42e93da62
LaoTrueNan <leonard.qiang@gmail.com>
2022-09-20 16:38:30 +0800

    new files

---
+++ src/com/byd/gzq/dao/PersonMapper.java
@@ -0,0 +1,14 @@
+package com.byd.gzq.dao;
+
+import com.byd.gzq.bean.Person;
+
+import java.util.List;
+
+/**
+ * @author 4466184
+ * @date 2022/9/20 16:13
+ */
+
+public interface PersonMapper {
+    List<Person> selectPersons();
+}
